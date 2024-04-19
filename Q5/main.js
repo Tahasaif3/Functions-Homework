97b99a8b0709da933b047780236f0904605bf218
Taha Saif <160765440+Tahasaif3@users.noreply.github.com>
2024-04-20 00:06:28 +0500

    Add files via upload

---
+++ Q5/main.js
@@ -0,0 +1,5 @@
+//Q5:Function displayMessage() prints "Hello World" string using console.log()function
+function displayMessage() {
+    console.log("Hello World!");
+}
+displayMessage();
---
+++ Q5/main.ts
@@ -0,0 +1,6 @@
+//Q5:Function displayMessage() prints "Hello World" string using console.log()function
+
+function displayMessage() {
+    console.log("Hello World!");
+}
+displayMessage();
